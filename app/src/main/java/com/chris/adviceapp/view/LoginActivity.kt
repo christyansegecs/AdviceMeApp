69879275d44db112ec7af9b2057ddc39d18a4a5e
christyansegecs <christyansegecs@hotmail.com>
2022-12-19 03:05:42 -0300

    Login Layout changes

--- app/src/main/java/com/chris/adviceapp/view/LoginActivity.kt
+++ app/src/main/java/com/chris/adviceapp/view/LoginActivity.kt
@@ -6,12 +6,18 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.chris.adviceapp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,6 +47,17 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.btnGoogle.setOnClickListener{
+            signInGoogle()
+        }
+
+        val gso = GoogleSignInOptions.Builder(
+            GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("810371366875-ruatceu4ca7sn3v16h08j6rr0iia7gvt.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
     }
 
     private fun loginWithFirebase(userEmail: String, password: String) {
@@ -64,6 +81,53 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    private fun signInGoogle() {
+        intent = googleSignInClient.signInIntent
+        startActivityForResult(intent, 1)
+    }
+
+
+    private fun loginGoogle(token: String) {
+        val credential = GoogleAuthProvider.getCredential(token,null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this) { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(
+                    applicationContext, "Login efetuado com sucesso",
+                    Toast.LENGTH_LONG
+                ).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(
+                    applicationContext, "Erro ao efetuar login",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+        if (requestCode == 1) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(intent).result
+            try {
+                account.idToken?.let { loginGoogle(it) }
+            } catch (exception : ApiException) {
+                Toast.makeText(
+                    applicationContext, "Erro ao efetuar login, nenhum usuário Google cadastrado no dispositivo",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            finally {
+                Toast.makeText(
+                    applicationContext, "Erro ao efetuar login, nenhum usuário Google cadastrado no dispositivo",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
         }
     }
 }
